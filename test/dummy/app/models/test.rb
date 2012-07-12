27566cd7e64728f3b8ef8bc16b14b75aef4aea27
Adam Gamble <adamgamble@gmail.com>
2012-07-11 21:47:40 -0500

    removed unneeed files

--- test/dummy/app/models/test.rb
+++
@@ -1,8 +0,0 @@
-class Test < ActiveRecord::Base
-  # attr_accessible :title, :body
-  #
-  keeps_score :if => { true },
-              :on => [:create],
-              :amount => { 1 },
-              :with => {|o| o.user }
-end
--- test/dummy/db/migrate/20120711203915_create_tests.rb
+++
@@ -1,9 +0,0 @@
-class CreateTests < ActiveRecord::Migration
-  def change
-    create_table :tests do |t|
-
-      t.integer :user_id
-      t.timestamps
-    end
-  end
-end
